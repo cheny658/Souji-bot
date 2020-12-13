93f5e50ac8c1dfacd490afa33c29fd7c4d39ce07
Yuuuuuika <827829263@qq.com>
2020-12-13 15:37:14 +0800

    Restruct

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+//
+// Created by Administrator on 2020/12/13.
+//
+
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  std::cout << "hello world" << std::endl;
 }
