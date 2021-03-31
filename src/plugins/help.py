b8c05c8b70c9085c343ec93349f7967359adf46f
Yuuuuuika <827829263@qq.com>
2021-03-31 11:49:02 +0800

    Updated help

--- src/plugins/help.py
+++ src/plugins/help.py
@@ -5,21 +5,5 @@ from image_processor import img_splice, img_save
 help_cmd = on_command('help')
 @help_cmd.handle()
 async def get_help(bot: Bot, event: Event, stat: dict):
-    info_box = []
-    info_box.append('总司bot是一个用于查询codeforces信息的机器人')
-    info_box.append('!info: 查询选手信息')
-    info_box.append('!setid: 绑定指令')
-    info_box.append('!unset: 解除绑定')
-    info_box.append('!infome: 查询绑定账号的信息')
-    info_box.append('!ct: 查询未来的比赛信息')
-    info_box.append('!sup: 打钱')
-    info_box.append('!roll: 随机数指令')
-    info_box.append('!ping: 网络测试指令')
-
-    info_box.append('新功能绝赞开发中...')
-
-    ret_img = img_splice(info_box)
-    file_name = 'help.png'
-    img_save(ret_img, file_name)
-    ret_msg = '[CQ:image,file=' + file_name + ']'
+    ret_msg = '[CQ:image,file=bot-help.png]'
     await bot.send(message=ret_msg, event=event)
--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@ cp data/images/sup.png ../go-cqhttp/data/images
 echo "Copied sup.png to service data"
 cp data/cqhttp-json/config.hjson ../go-cqhttp/
 echo "Copied config.hjson to service"
+cp data/images/bot-help.png ../go-cqhttp/data/images
+echo "Copied bot-help.png to service data"
