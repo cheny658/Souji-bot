d229a7ca83a4e7550c1a6f08aaafa36147d010e1
Yuuuuuika <827829263@qq.com>
2021-04-01 13:37:49 +0800

    Updated sup

--- src/plugins/support.py
+++ src/plugins/support.py
@@ -4,5 +4,7 @@ from nonebot.adapters.cqhttp import Bot, Event
 sup_cmd = on_command('sup')
 @sup_cmd.handle()
 async def get_sup(bot: Bot, event: Event, stat: dict):
-    ret_msg = '[CQ:image,file=sup.png][CQ:record,file=thx.png]'
+    ret_msg = '[CQ:image,file=sup.png]'
     await bot.send(message=ret_msg, event=event)
+    ret_msg = '[CQ:record,file=thx.png]'
+    await bot.send(message=ret_msg, event=event
